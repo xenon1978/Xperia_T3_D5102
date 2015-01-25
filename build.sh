1f26761138ac608039ff99b2dc31dbda60595b1f
xenon1978 <dj.prashanth1978@yahoo.co.in>
2015-01-25 15:33:48 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-# Modify the following variable if you want to build
-export CROSS_COMPILE="/home/prashi/android/android_prebuilt_toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-"
-export ARCH=arm
-export SUBARCH=arm
-make proj_SEAGULL_3G_defconfig
-make -j8 CONFIG_NO_ERROR_ON_MISMATCH=y
